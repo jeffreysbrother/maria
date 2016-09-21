5a93746e15604850948ba4b363923c0f129fbb6b
James Cool <jeffreysbrother@gmail.com>
2016-09-21 13:07:42 -0700

    centering stuff

--- src/js/templates/TodosInputTemplate.js
+++ src/js/templates/TodosInputTemplate.js
@@ -1,5 +1,6 @@
 checkit.TodosInputTemplate =
     '<div class="TodosInput">' +
+        // '<h1>todo tracker</h1>' + 
         '<input class="content" placeholder="What needs to be done?" type="text">' +
         // '<span class="instructions">Press enter to add this task.</span>' +
     '</div>';
