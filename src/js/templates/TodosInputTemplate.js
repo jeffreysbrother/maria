1a04eb61b723318287116d9da2688017ce6a56df
James Cool <jeffreysbrother@gmail.com>
2016-09-21 12:57:43 -0700

    add viewport meta tag and get ul to center

--- src/js/templates/TodosInputTemplate.js
+++ src/js/templates/TodosInputTemplate.js
@@ -1,5 +1,5 @@
 checkit.TodosInputTemplate =
     '<div class="TodosInput">' +
         '<input class="content" placeholder="What needs to be done?" type="text">' +
-        '<span class="instructions">Press enter to add this task.</span>' +
+        // '<span class="instructions">Press enter to add this task.</span>' +
     '</div>';
